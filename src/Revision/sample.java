393048f51f7d7eb4b1e07ad6928fdaa6d0743ec1
Lenovo <Lenovo@DESKTOP-CQQ66HC>
2020-12-07 13:22:33 +0530

    [Minakshi]: Test git

---
+++ src/Revision/sample.java
@@ -0,0 +1,19 @@
+package Revision;
+
+import java.util.Scanner;
+
+public class sample {
+	public static void main(String[] args) {
+		
+	
+	
+	Scanner s=new Scanner(System.in);
+	String str=s.next();
+System.out.println(str);
+
+Scanner s1=new Scanner(System.in);
+System.out.println("Enter boolean value");
+
+boolean a=s1.hasNext();
+System.out.println(a);
+}}
